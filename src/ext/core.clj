2cbe6bdc407112338140b6369e4d1bdf3027b56c
Jim Brusstar <jim.brusstar@gmail.com>
2015-04-01 14:57:37 -0700

    nth

--- src/ext/core.clj
+++ src/ext/core.clj
@@ -1,6 +1,31 @@
-(ns ext.core)
+(ns ext.core
+  (:refer-clojure :rename {nth nth-builtin}))
+
+(defn- nth-impl [coll index]
+  (let [coll-count (count coll)]
+    (when (or (= coll-count 0)
+              (not (<= (- coll-count) index (dec coll-count))))
+      (throw (IndexOutOfBoundsException.)))
+    (nth-builtin coll (mod index coll-count))))
+
+(defn nth
+  "Just like the built-in clojure.core/nth, except this also understands
+  how to wrap negative indices.
+
+  For example:
+
+      (nth [1 2 3 4 5] 2)  => 3
+      (nth [1 2 3 4 5] -2) => 4
+  "
+  ([coll index]
+    (nth-impl coll index))
+  ([coll index not-found]
+    (try
+      (nth-impl coll index)
+      (catch IndexOutOfBoundsException e
+        not-found))))
 
 (defn second-to-last
   "Return the second-to-last item in coll."
   [coll]
-  (nth coll (dec (dec (count coll)))))
+  (nth coll -2))
--- test/ext/core_test.clj
+++ test/ext/core_test.clj
@@ -1,6 +1,29 @@
 (ns ext.core-test
+  (:refer-clojure :exclude [nth])
   (:require [clojure.test :refer :all]
             [ext.core :refer :all]))
+
+(deftest nth-test
+  (let [v [1 2 3 4 5]]
+    (testing "valid positive indices"
+      (is (= 1 (nth v 0)))
+      (is (= 2 (nth v 1)))
+      (is (= 5 (nth v 4)))
+      (is (= 5 (nth v 4 :default))))
+    (testing "invalid positive indices"
+      (is (thrown? IndexOutOfBoundsException (nth v 5)))
+      (is (= :default (nth v 5 :default))))
+    (testing "valid negative indices"
+      (is (= 5 (nth v -1)))
+      (is (= 4 (nth v -2)))
+      (is (= 1 (nth v -5)))
+      (is (= 1 (nth v -5 :default))))
+    (testing "invalid negative indices"
+      (is (thrown? IndexOutOfBoundsException (nth v -6)))
+      (is (= :default (nth v -6 :default)))))
+  (testing "empty collection"
+    (is (thrown? IndexOutOfBoundsException (nth [] 0)))
+    (is (= :default (nth [] 0 :default)))))
 
 (deftest second-to-last-test
   (is (= 4 (second-to-last '(1 2 3 4 5))))
