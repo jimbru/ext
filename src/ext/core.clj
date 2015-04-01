6633905d8a8c9e4090340aa4fb74a508fe87624c
Jim Brusstar <jim.brusstar@gmail.com>
2015-04-01 15:30:20 -0700

    nth without try/catch

--- src/ext/core.clj
+++ src/ext/core.clj
@@ -1,12 +1,17 @@
 (ns ext.core
   (:refer-clojure :rename {nth nth-builtin}))
 
-(defn- nth-impl [coll index]
+(defn- nth-impl [coll index & {:keys [throw? not-found]}]
   (let [coll-count (count coll)]
-    (when (or (= coll-count 0)
-              (not (<= (- coll-count) index (dec coll-count))))
-      (throw (IndexOutOfBoundsException.)))
-    (nth-builtin coll (mod index coll-count))))
+    (if (or (= coll-count 0)
+            (not (<= (- coll-count) index (dec coll-count))))
+      (if throw?
+        (throw (IndexOutOfBoundsException.))
+        not-found)
+      (let [mod-index (mod index coll-count)]
+        (if throw?
+          (nth-builtin coll mod-index)
+          (nth-builtin coll mod-index not-found))))))
 
 (defn nth
   "Just like the built-in clojure.core/nth, except this also understands
@@ -18,12 +23,9 @@
       (nth [1 2 3 4 5] -2) => 4
   "
   ([coll index]
-    (nth-impl coll index))
+    (nth-impl coll index :throw? true))
   ([coll index not-found]
-    (try
-      (nth-impl coll index)
-      (catch IndexOutOfBoundsException e
-        not-found))))
+    (nth-impl coll index :throw? false :not-found not-found)))
 
 (defn second-to-last
   "Return the second-to-last item in coll."
